0dd8739c3618d57e2df5e3e3af894f73ef848c3c
Gauransh Soni <63138889+picografix@users.noreply.github.com>
2020-05-26 01:30:03 +0530

    wallpaper update

--- application.py
+++ application.py
@@ -34,8 +34,36 @@ def bot():
         msg.body('Love <3')
         msg.media(ans)
         responded = True
+    if 'wallpaper' in incoming_msg:
+            l=incoming_message.split()
+            url=l[1]
+        try: 
+            from googlesearch import search 
+        except ImportError:  
+            print("No module named 'google' found") 
+        
+        # to search 
+        query = url+" unsplash"
+        
+        for j in search(query, tld="co.in", num=1, stop=4, pause=2):
+            if "https://unsplash.com/s/photos" in j: 
+                url=j 
+        a=urllib.request.urlopen(url,context=ctx).read()
+        soup=bs(a,'html.parser')
+        L=soup.find_all('a',{'title':"Download photo"})
+        x=randint(1,len(L)-1)
+        count=0
+        for i in L:
+            count+=1
+            if count==x:
+                alink=i.get('href')
+                break
+            else:
+                continue
+        msg.media(alink)
+        responded=True
     if not responded:
-        msg.body('I only know about famous quotes and cats, sorry!')
+        msg.body('I only know about famous quotes and cats, sorry! (ver 1.0.2)')
     return str(resp)
 
 
