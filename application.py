95f0caf09ffa2cd77c068f4b38a17283fe6163af
Gauransh Soni <63138889+picografix@users.noreply.github.com>
2020-05-26 17:59:02 +0530

    Update application.py

--- application.py
+++ application.py
@@ -101,35 +101,6 @@ def bot():
         msg.body(returnString)
         msg.media(img)  
         responded = True
-    if 'que' in incoming_msg:
-        import urllib.request, urllib.parse, urllib.error
-        import xml.etree.ElementTree as ET
-        import ssl
-        from bs4 import BeautifulSoup as bs
-
-        ctx = ssl.create_default_context()
-        ctx.check_hostname = False
-        ctx.verify_mode = ssl.CERT_NONE
-
-        url = ' '.join(incoming_msg.split()[1:])
-
-        try: 
-            from googlesearch import search 
-        except ImportError:  
-            print("No module named 'google' found") 
-          
-        # to search 
-        query = url+" stackoverflow"
-          
-        for j in search(query, tld="co.in", num=1, stop=1, pause=2): 
-            url=j 
-        a=urllib.request.urlopen(url,context=ctx).read()
-        soup=bs(a,'html.parser')
-        L=soup.find_all('div',{'class':'post-text'})
-        i=L[1]
-        msg.body(i.text)
-        print(i.text)
-        responded = True
     if not responded:
         msg.body('I only know about famous quotes and cats, sorry! (ver 1.0.2)')
     return str(resp)
