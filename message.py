e43085530242580b4b759d91a8eb5162dfc0d04d
picografix <picografix@gmail.com>
2021-05-08 03:53:56 +0530

    security

--- message.py
+++ message.py
@@ -1,7 +1,7 @@
 from twilio.rest import Client 
  
 account_sid = 'ACaa40d5fd76e3d54bdde5807fce65adbb' 
-auth_token = 'd36988b8758ca887b9300da3621def27' 
+auth_token = 'a67f9ecae87624705b59b7816361cf05' 
 client = Client(account_sid, auth_token) 
  
 message = client.messages.create( 
